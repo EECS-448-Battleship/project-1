69915705e1a753d1ab7926fe470f01e3c5024db0
garrettmills <shout@garrettmills.dev>
2020-09-07 14:34:43 -0500

    Create game state service placeholder class

---
+++ src/services/GameState.service.js
@@ -0,0 +1,10 @@
+/**
+ * Singleton service for managing the state of the game.
+ */
+export class GameStateService {
+
+}
+
+// Export a single instance, so it can be shared by all files
+const game_state = new GameStateService()
+export default game_state
