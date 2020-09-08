a803c6937d20979da1834d3031dfbd6306e153c4
garrettmills <shout@garrettmills.dev>
2020-09-07 21:04:28 -0500

    Add custom error classes for state and placement (#2)

---
+++ src/module/errors.js
@@ -0,0 +1,13 @@
+/**
+ * Placeholder class for an error that is thrown when a ship is placed
+ * in an invalid position.
+ * @extends Error
+ */
+export class InvalidShipPlacementError extends Error {}
+
+/**
+ * Error thrown when the program tries to advance the state, but it is
+ * invalid.
+ * @extends Error
+ */
+export class InvalidAdvanceStateError extends Error {}
